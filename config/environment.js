45c0d99112138c3c5e71aa3d36f45a34f0ffd5fd
Garthar <garthar2345@gmail.com>
2020-08-16 13:47:48 +0300

    16.18.20

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZ2FydGhhciIsImEiOiJja2R3eHl0YjMyd2xwMnh0dmV5anFkbmVxIn0.4RoCZADGwLRAYqONhZ0MJg';
+
   return ENV;
 };
