19c74bd74c8facae87820791c2c74dca7e72f4df
Garthar <garthar2345@gmail.com>
2020-08-28 18:16:48 +0300

    27.08.20

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -1,6 +1,6 @@
-import { module, test } from 'qunit';
-import { click, visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import {module, test} from 'qunit';
+import {click, find, visit, currentURL} from '@ember/test-helpers';
+import {setupApplicationTest} from 'ember-qunit';
 
 module('Acceptance | super rentals', function(hooks) {
   setupApplicationTest(hooks);
@@ -35,6 +35,17 @@ module('Acceptance | super rentals', function(hooks) {
     assert.dom('h1').containsText('Super Rentals');
     assert.dom('h2').containsText('Grand Old Mansion');
     assert.dom('.rental.detailed').exists();
+    assert.dom('.share.button').hasText('Share on Twitter');
+
+    let button = find('.share.button');
+
+    let tweetURL = new URL(button.href);
+    assert.equal(tweetURL.host, 'twitter.com');
+
+    assert.equal(
+      tweetURL.searchParams.get('url'),
+      `${window.location.origin}/rentals/grand-old-mansion`
+    );
   });
 
   test('visiting /about', async function (assert) {
